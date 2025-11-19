c9e75dde1a5f61e2dd91f60f06adda4192766d81
ritesh <vermaritesh675@gmail.com>
2025-11-19 12:50:01 +0530

    Change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,33 +5,48 @@ import { useLocation } from "react-router-dom";
 const Footer = () => {
   const location = useLocation();
   const isHomePage = location.pathname === "/";
+
   return (
     <>
-      <footer className={isHomePage ? 'homePage_footer otherPage_footer': "otherPage_footer"}>
-        <div className="container text-white">
-          <h4>LUXURY RENTALS</h4>
-          <p>
+      <footer
+        className={`${isHomePage ? "homePage_footer otherPage_footer" : "otherPage_footer"}`}
+        style={{ color: "white", opacity: 1 }}
+      >
+        <div className="container" style={{ color: "white", opacity: 1 }}>
+          <h4 style={{ color: "white", opacity: 1 }}>LUXURY RENTALS</h4>
+
+          <p style={{ color: "white", opacity: 1 }}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum et
             minus voluptatibus corporis eligendi quasi iure nihil eum reiciendis
             quod.
           </p>
+
           <ul>
             <li>
-              <Link to={"/"}>Home</Link>
+              <Link to="/" style={{ color: "white", opacity: 1 }}>
+                Home
+              </Link>
             </li>
             <li>
-              <Link to={"/termsandconditions"}>Terms&Conditions</Link>
+              <Link to="/termsandconditions" style={{ color: "white", opacity: 1 }}>
+                Terms & Conditions
+              </Link>
             </li>
             <li>
-              <Link to={"/"}>Contact</Link>
+              <Link to="/" style={{ color: "white", opacity: 1 }}>
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
-        <div className="container">
-          <h4>Connect with us</h4>
-          <p>+00 000 000 00</p>
-          <p>zk@gmail.com</p>
-          <p>© All Rights Reserved By CodeWithZeeshu.</p>
+
+        <div className="container" style={{ color: "white", opacity: 1 }}>
+          <h4 style={{ color: "white", opacity: 1 }}>Connect with us</h4>
+          <p style={{ color: "white", opacity: 1 }}>+91 9260943685</p>
+          <p style={{ color: "white", opacity: 1 }}>riteshverma1055@gmail.com</p>
+          <p style={{ color: "white", opacity: 1 }}>
+            © All Rights Reserved By Ritesh.
+          </p>
         </div>
       </footer>
     </>
